d6caf3a408d61d26d3ccbd3110e05795a5fcce48
VoidX <voidxb@gmail.com>
2019-05-16 02:33:29 +0200

    Tester class with first test

---
+++ RoomEditor/Tester.cs
@@ -0,0 +1,14 @@
+﻿using HomeEditor.Elements;
+using System;
+
+namespace HomeEditor {
+    public static class Tester {
+        public static void EmptyHouseForADay() {
+            Sensor target = Sensor.Random;
+            for (int minutesBack = 30 * 60; minutesBack >= 0; --minutesBack) { // Send no movement info for 30 hours
+                DateTime targetTime = DateTime.Now - TimeSpan.FromMinutes(minutesBack);
+                target.DataReceived(new SensorData(targetTime));
+            }
+        }
+    }
+}
